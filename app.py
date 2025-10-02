221aff2dfbfbb0a7050d24ac2e8583256735e8a6
Jocelyn Garcia <221137@utags.edu.mx>
2025-10-01 19:53:00 -0600

    PROYECTO PRIMER PARCIAL JOCELYN

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return {
+        "materia": "Automatización de Infraestructura Digital",
+        "profesor": "Froylan Alonso Perez", 
+        "alumno": "Jocelyn Guadalupe Garcia Martinez",
+        "carrera": "Ingeniería en Redes Inteligentes y Ciberseguridad"
+    }
+
+@app.route('/health')
+def health_check():
+    return {"status": "healthy", "message": "App funcionando correctamente"}
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
